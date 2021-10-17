590a2c6c31044d9ff9fe2849acad56c9d0522f32
Emilio González Martínez <emiliogonmar2002@gmail.com>
2021-10-17 15:13:50 +0000

    Second

--- numarch.c
+++ numarch.c
@@ -1,12 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
 int main(){
     int fd[2];
     pipe(fd);
-    int pif = fork();
-    if(pid == 0){
+    int pid=fork();
 
-    }
-    
-    else{
+    if(pid==0){
+        close(0);
+        dup2(1,fd[1]);
+        execlp("wc","wc","-l",NULL);
+    }else{
+        close(1);
+        dup2(1,fd[1]);
+        execlp("ls","ls",NULL);
 
     }
 
